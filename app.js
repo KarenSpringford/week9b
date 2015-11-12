553415ee13abed6b940a344f710902cd03871d80
KarenSpringford <jkspringford@gmail.com>
2015-11-12 10:49:39 -0500

    First Commit

---
+++ app.js
@@ -0,0 +1,10 @@
+// declare my function
+function init() {
+    var myNum = 5;
+    console.log("My App is running...");
+    console.log("My number is " + myNum);
+    return myNum;
+}
+// call my function
+console.log("The number is: " + init());
+//# sourceMappingURL=app.js.map
---
+++ app.ts
@@ -0,0 +1,16 @@
+
+
+// declare my function
+function init():Number {
+	var myNum:Number = 5; 
+	
+	console.log("My App is running...");
+	console.log("My number is " + myNum);
+	
+	return myNum;
+}
+
+// call my function
+console.log("The number is: " + init());
+
+
